89becfd9de6c19ab4aed6ad4c568612aa08e9063
Dominik Gabela <gabela.dominik@gmail.com>
2025-07-01 15:51:56 +0200

    Contact margin

--- delegate-call/src/app/contact/page.tsx
+++ delegate-call/src/app/contact/page.tsx
@@ -1,8 +1,25 @@
 import React from "react";
-import ContactInformation from "../../../components/ContactInformation";
 
 const ContactPage = () => {
-  return <ContactInformation />;
+  return (
+    <div className="h-[80vh] text-center mt-24">
+      <h1 className="text-5xl font-extrabold mb-6 tracking-tight">
+          Get in Touch
+        </h1>
+        <div className="flex items-center justify-center">
+          <p className="max-w-xl text-xl leading-relaxed text-center">
+            Have questions or want to collaborate? Email us directly at{' '}
+            <a
+              href="mailto:matecoim@gmail.com"
+              className="font-bold underline inline-block text-2xl  animate-pulse text-center"
+            >
+              matecoim@gmail.com
+            </a>
+            .
+          </p>
+        </div>
+    </div>
+  )
 };
 
 export default ContactPage;
